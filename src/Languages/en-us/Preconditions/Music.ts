01699c9223fa6e3ec24a65fbb04921a2ed4e19e0
kagchi <yonuxbot@gmail.com>
2023-03-22 18:53:34 +0700

    feat: add new locales

--- src/Languages/en-us/Preconditions/Music.ts
+++ src/Languages/en-us/Preconditions/Music.ts
@@ -9,4 +9,5 @@ export class EnUsPreconditionsMusic extends LanguagePiece {
     public NOT_ON_SAME_VOICE = (): string => "You must on same voice channel with me, in order to execute this action.";
     public NOT_ON_VOICE = (): string => "You must on voice channel, in order to execute this action.";
     public QUEUE_NOT_AVAILABLE = (): string => "You have not enqueue a track.";
+    public NOT_DJ = (): string => "You must be DJ, in order to execute this action.";
 }
