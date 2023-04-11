1d6a78e3fb1001573599766c83d5f530b854d677
kagchi <yonuxbot@gmail.com>
2023-04-11 16:42:17 +0700

    fix: make the translation on top-level

--- src/Languages/en-us/Preconditions/index.ts
+++ src/Languages/en-us/Preconditions/index.ts
@@ -1,6 +1,7 @@
 import { LanguageOptions, LanguagePiece } from "@nezuchan/i18n";
 import { ApplyOptions } from "@nezuchan/decorators";
 import { EnUsPreconditionsMusic } from "./Music.js";
+import { inlineCode } from "@discordjs/builders";
 
 @ApplyOptions<LanguageOptions>({
     name: "en-US/preconditions"
@@ -10,4 +11,8 @@ export class EnUsPreconditions extends LanguagePiece {
     public get music(): EnUsPreconditionsMusic {
         return this.container.stores.get("languages").get("en-US/preconditions/music") as EnUsPreconditionsMusic;
     }
+
+    public WAIT_FOR_VOTE = (time: number | string): string => `You have ${inlineCode(String(time))} second(s) to vote for the bot https://top.gg/bot/616169470293049344/vote or purchase premium to bypass`;
+    public NOT_VOTED = (): string => "To use this command you must vote for the bot https://top.gg/bot/616169470293049344/vote or purchase premium to bypass";
+    public NOT_PREMIUM = (): string => "You must purchase premium to use this command";
 }
