0c0de74007c243b52ebbb08e329f5cbd6815452c
kagchi <yonuxbot@gmail.com>
2023-04-13 19:13:44 +0700

    feat: add new locales

--- src/Languages/en-us/Commands/Configuration.ts
+++ src/Languages/en-us/Commands/Configuration.ts
@@ -1,5 +1,6 @@
 import { LanguageOptions, LanguagePiece } from "@nezuchan/i18n";
 import { ApplyOptions } from "@nezuchan/decorators";
+import { inlineCode } from "@discordjs/builders";
 
 @ApplyOptions<LanguageOptions>({
     name: "en-US/commands/configuration"
@@ -8,4 +9,6 @@ import { ApplyOptions } from "@nezuchan/decorators";
 export class EnUsCommandsConfiguration extends LanguagePiece {
     public DJ_ALREADY_ADDED = (): string => "The user or role is already added.";
     public DJ_NOT_USER = (): string => "You must provide a valid user.";
+    public LOCALES_CURRENT = (locale: string): string => `The current locale is set to ${inlineCode(locale)}.`;
+    public LOCALES_INVALID = (): string => "You must provide a valid locale.";
 }
