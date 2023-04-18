0eb034b84df018a16b4f4ba5c1da585f626e25b6
kagchi <yonuxbot@gmail.com>
2023-04-18 11:21:08 +0700

    chore: update locales

--- src/Languages/en-us/Commands/Configuration.ts
+++ src/Languages/en-us/Commands/Configuration.ts
@@ -1,6 +1,8 @@
 import { LanguageOptions, LanguagePiece } from "@nezuchan/i18n";
 import { ApplyOptions } from "@nezuchan/decorators";
 import { inlineCode } from "@discordjs/builders";
+import { Emoji } from "@nezuchan/constants";
+import { stripIndents } from "common-tags";
 
 @ApplyOptions<LanguageOptions>({
     name: "en-US/commands/configuration"
@@ -12,4 +14,27 @@ export class EnUsCommandsConfiguration extends LanguagePiece {
     public LOCALES_CURRENT = (locale: string): string => `The current locale is set to ${inlineCode(locale)}.`;
     public LOCALES_INVALID = (): string => "You must provide a valid locale.";
     public LOCALES_UPDATED = (locale: string): string => `The locale has been updated to ${inlineCode(locale)}.`;
+    public VOLUME = (volume: number | string): string => `The default volume configuration has been set to ${inlineCode(String(volume))}.`;
+    public VOLUME_VIEW = (volume: number | string): string => `The default volume configuration is set to ${inlineCode(String(volume))}.`;
+    public LIMIT_REACHED = (limit: number | string): string => `You have reached the limit of ${inlineCode(String(limit))} configuration.`;
+    public ENABLED = (config: string): string => `The ${inlineCode(config)} configuration has been enabled.`;
+    public DISABLED = (config: string): string => `The ${inlineCode(config)} configuration has been disabled.`;
+    public VIEW = (config: string, state: boolean): string => `The ${inlineCode(config)} configuration is set to ${state ? "enabled" : "disabled"}.`;
+
+    public DJ = (users: string[], roles: string []): string => stripIndents`
+        ${Emoji.Ok} | Updated the DJ configuration.
+
+        DJ Users: ${users.length ? users.join(", ") : "none"}
+        DJ Roles: ${roles.length ? roles.join(", ") : "none"}
+    `;
+
+    public DJ_VIEW = (users: string[], roles: string [], state: boolean): string => stripIndents`
+        ${state ? Emoji.Ok : Emoji.No} | The DJ configuration is set to ${state ? "enabled" : "disabled"}.
+
+        DJ Users: ${users.length ? users.join(", ") : "none"}
+        DJ Roles: ${roles.length ? roles.join(", ") : "none"}
+    `;
+
+    public LIMIT = (limit: number | string): string => `You only allowed to add ${inlineCode(String(limit))} config.`;
+    public INVALID = (): string => "You must provide a valid config.";
 }
--- src/Languages/en-us/Utility/index.ts
+++ src/Languages/en-us/Utility/index.ts
@@ -1,8 +1,6 @@
 import { LanguageOptions, LanguagePiece } from "@nezuchan/i18n";
 import { ApplyOptions } from "@nezuchan/decorators";
 import { inlineCode } from "@discordjs/builders";
-import { stripIndents } from "common-tags";
-import { Emoji } from "@nezuchan/constants";
 
 @ApplyOptions<LanguageOptions>({
     name: "en-US/utility"
@@ -15,26 +13,4 @@ export class EnUsUtility extends LanguagePiece {
     public INVALID_RANGE = (min: number | string, max: number | string): string => `You must provide a valid range. between ${inlineCode(String(min))} and ${inlineCode(String(max))}.`;
     public MESSAGE_COMMANDS_NOT_SUPPORTED = (): string => "Message commands are no longer supported.";
     public SUB_COMMAND_NOT_FOUND = (subCommand: string): string => `Sub command \`${subCommand}\` not found.`;
-    public CONFIG_LIMIT_REACHED = (limit: number | string): string => `You have reached the limit of ${inlineCode(String(limit))} configuration.`;
-    public CONFIG_ENABLED = (config: string): string => `The ${inlineCode(config)} configuration has been ${this.ENABLED()}.`;
-    public CONFIG_DISABLED = (config: string): string => `The ${inlineCode(config)} configuration has been ${this.DISABLED()}.`;
-    public CONFIG_VIEW = (config: string, state: boolean): string => `The ${inlineCode(config)} configuration is set to ${state ? this.ENABLED() : this.DISABLED()}.`;
-    public CONFIG_VOLUME = (volume: number | string): string => `The default volume configuration has been set to ${inlineCode(String(volume))}.`;
-    public CONFIG_VOLUME_VIEW = (volume: number | string): string => `The default volume configuration is set to ${inlineCode(String(volume))}.`;
-    public CONFIG_DJ = (users: string[], roles: string []): string => stripIndents`
-        ${Emoji.Ok} | Updated the DJ configuration.
-
-        DJ Users: ${users.length ? users.join(", ") : "none"}
-        DJ Roles: ${roles.length ? roles.join(", ") : "none"}
-    `;
-
-    public CONFIG_DJ_VIEW = (users: string[], roles: string [], state: boolean): string => stripIndents`
-        ${state ? Emoji.Ok : Emoji.No} | The DJ configuration is set to ${state ? this.ENABLED() : this.DISABLED()}.
-
-        DJ Users: ${users.length ? users.join(", ") : "none"}
-        DJ Roles: ${roles.length ? roles.join(", ") : "none"}
-    `;
-
-    public CONFIG_LIMIT = (limit: number | string): string => `You only allowed to add ${inlineCode(String(limit))} config.`;
-    public CONFIG_INVALID = (): string => "You must provide a valid config.";
 }
